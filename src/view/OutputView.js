4231c76fee49f4b4d0f3609aba940540e2e8ae42
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-03 20:05:08 +0900

    feat: Implement OutputView.printPurchaseResult

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,6 +1,13 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const OutputView = {
+  printPurchaseResult(lottos) {
+    Console.print(`\n${lottos.length}개를 구매했습니다.`);
+    lottos.forEach(lotto => {
+      Console.print(`[${lotto.getNumbers().join(", ")}]`);
+    });
+  },
+
   printError(message) {
     Console.print(message);
   },
