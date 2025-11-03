3859f51bb739731d3301c32e1227a2c4a7007bc9
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-03 16:52:18 +0900

    test: Add tests for match method in WinningNumbers

--- __tests__/WinningNumbersTest.js
+++ __tests__/WinningNumbersTest.js
@@ -18,4 +18,13 @@ describe("WinningNumbers 클래스 테스트", () => {
       new WinningNumbers([1, 2, 3, 4, 5, 6], 6);
     }).toThrow("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
   });
+
+  test("match 메서드는 일치하는 번호 개수와 보너스 번호 일치 여부를 반환한다.", () => {
+    const winningNumbers = new WinningNumbers([1, 2, 3, 4, 5, 6], 7);
+    const lotto1 = { countMatches: () => 3, hasNumber: () => false }; // 모의 Lotto 객체
+    const lotto2 = { countMatches: () => 5, hasNumber: () => true }; // 모의 Lotto 객체
+
+    expect(winningNumbers.match(lotto1)).toEqual({ matchCount: 3, hasBonus: false });
+    expect(winningNumbers.match(lotto2)).toEqual({ matchCount: 5, hasBonus: true });
+  });
 });
