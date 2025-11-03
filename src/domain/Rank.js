01b1b9aa2dcdb611f69b99689b67a40dfb1bce5b
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-03 16:57:36 +0900

    feat: Implement Rank getter methods

--- src/domain/Rank.js
+++ src/domain/Rank.js
@@ -2,26 +2,38 @@ const Rank = {
   FIRST: {
     prize: 2_000_000_000,
     description: '6개 일치',
+    getPrize() { return this.prize; },
+    getDescription() { return `${this.description} (${this.prize.toLocaleString()}원)`; },
   },
   SECOND: {
     prize: 30_000_000,
     description: '5개 일치, 보너스 볼 일치',
+    getPrize() { return this.prize; },
+    getDescription() { return `${this.description} (${this.prize.toLocaleString()}원)`; },
   },
   THIRD: {
     prize: 1_500_000,
     description: '5개 일치',
+    getPrize() { return this.prize; },
+    getDescription() { return `${this.description} (${this.prize.toLocaleString()}원)`; },
   },
   FOURTH: {
     prize: 50_000,
     description: '4개 일치',
+    getPrize() { return this.prize; },
+    getDescription() { return `${this.description} (${this.prize.toLocaleString()}원)`; },
   },
   FIFTH: {
     prize: 5_000,
     description: '3개 일치',
+    getPrize() { return this.prize; },
+    getDescription() { return `${this.description} (${this.prize.toLocaleString()}원)`; },
   },
   NONE: {
     prize: 0,
     description: '낙첨',
+    getPrize() { return this.prize; },
+    getDescription() { return this.description; },
   },
 
   of(matchCount, hasBonus) {
