489fd7f58f5d52aed8e368488fc56b8a42ae7c89
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-03 19:57:02 +0900

    feat: Implement WinningService.calculateStatistics

--- src/service/WinningService.js
+++ src/service/WinningService.js
@@ -7,6 +7,16 @@ const WinningService = {
       return Rank.of(matchCount, hasBonus);
     });
   },
+
+  calculateStatistics(ranks) {
+    const statistics = new Map(Rank.values().map(rank => [rank, 0]));
+    ranks.forEach(rank => {
+      if (rank !== Rank.NONE) {
+        statistics.set(rank, statistics.get(rank) + 1);
+      }
+    });
+    return statistics;
+  },
 };
 
 export default WinningService;
