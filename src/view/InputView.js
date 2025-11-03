53ed9fed9dbc31d02c2be3cd36cdd47dde07c115
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-03 20:28:52 +0900

    fix: Prevent infinite loop on closed input stream

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
 import PurchaseAmountValidator from "../validator/PurchaseAmountValidator.js";
+import WinningNumbersValidator from "../validator/WinningNumbersValidator.js";
+import BonusNumberValidator from "../validator/BonusNumberValidator.js";
 import OutputView from "./OutputView.js";
 
 const InputView = {
@@ -7,10 +9,15 @@ const InputView = {
     while (true) {
       try {
         const input = await Console.readLineAsync("구입금액을 입력해 주세요.\n");
+        this.#handleEndOfInput(input);
         PurchaseAmountValidator.validate(input);
         return Number(input);
       } catch (error) {
-        OutputView.printError(error.message);
+        if (this.#isRetryableError(error)) {
+          OutputView.printError(error.message);
+          continue;
+        }
+        throw error;
       }
     }
   },
@@ -19,10 +26,15 @@ const InputView = {
     while (true) {
       try {
         const input = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
+        this.#handleEndOfInput(input);
         WinningNumbersValidator.validate(input);
         return WinningNumbersValidator.parse(input);
       } catch (error) {
-        OutputView.printError(error.message);
+        if (this.#isRetryableError(error)) {
+          OutputView.printError(error.message);
+          continue;
+        }
+        throw error;
       }
     }
   },
@@ -31,12 +43,27 @@ const InputView = {
     while (true) {
       try {
         const input = await Console.readLineAsync("\n보너스 번호를 입력해 주세요.\n");
+        this.#handleEndOfInput(input);
         BonusNumberValidator.validate(input, winningNumbers);
         return Number(input);
       } catch (error) {
-        OutputView.printError(error.message);
+        if (this.#isRetryableError(error)) {
+          OutputView.printError(error.message);
+          continue;
+        }
+        throw error;
       }
     }
+  },
+
+  #handleEndOfInput(input) {
+    if (input === undefined) {
+      throw new Error("입력 스트림이 예기치 않게 종료되었습니다.");
+    }
+  },
+
+  #isRetryableError(error) {
+    return error.message.startsWith("[ERROR]");
   },
 };
 
