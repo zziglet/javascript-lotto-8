7b8973f5a67d5bc826b390cb3d68219d140ec157
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-03 20:04:22 +0900

    feat: Implement InputView.readWinningNumbers

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -14,6 +14,18 @@ const InputView = {
       }
     }
   },
+
+  async readWinningNumbers() {
+    while (true) {
+      try {
+        const input = await Console.readLineAsync("\n당첨 번호를 입력해 주세요.\n");
+        WinningNumbersValidator.validate(input);
+        return WinningNumbersValidator.parse(input);
+      } catch (error) {
+        OutputView.printError(error.message);
+      }
+    }
+  },
 };
 
 export default InputView;
