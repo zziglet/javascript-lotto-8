f7782deb600c4fffe2e25d619f5dd56452aca6ae
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-03 16:51:45 +0900

    test: Add validation test for duplicate bonus number in WinningNumbers

--- __tests__/WinningNumbersTest.js
+++ __tests__/WinningNumbersTest.js
@@ -12,4 +12,10 @@ describe("WinningNumbers 클래스 테스트", () => {
       new WinningNumbers([1, 2, 3, 4, 5, 6], 46);
     }).toThrow("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
   });
+
+  test("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.", () => {
+    expect(() => {
+      new WinningNumbers([1, 2, 3, 4, 5, 6], 6);
+    }).toThrow("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+  });
 });
