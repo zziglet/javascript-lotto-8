01dfa44df2a44bd6b52ef5e55ad79c6958bcf2bd
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-03 20:12:52 +0900

    refactor: Connect LottoGameController to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import LottoGameController from "./controller/LottoGameController.js";
+
 class App {
-  async run() {}
+  async run() {
+    const controller = new LottoGameController();
+    await controller.run();
+  }
 }
 
 export default App;
