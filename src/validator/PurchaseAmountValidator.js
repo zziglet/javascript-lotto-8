0c888b7970c95b3def0b25b3717ffee1aa430507
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-03 19:14:53 +0900

    feat: Implement validation for purchase amount unit and positivity

--- src/validator/PurchaseAmountValidator.js
+++ src/validator/PurchaseAmountValidator.js
@@ -3,6 +3,10 @@ const PurchaseAmountValidator = {
     if (isNaN(input)) {
       throw new Error("[ERROR] 구입 금액은 숫자여야 합니다.");
     }
+    const amount = Number(input);
+    if (amount <= 0 || amount % 1000 !== 0) {
+      throw new Error("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+    }
   },
 };
 
