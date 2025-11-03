059b56d40f8218a39d387629c12c3ec62f023721
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-03 19:09:59 +0900

    test: Add validation for non-numeric purchase amount

---
+++ __tests__/validator/PurchaseAmountValidatorTest.js
@@ -0,0 +1,9 @@
+import PurchaseAmountValidator from "../../../src/validator/PurchaseAmountValidator.js";
+
+describe("PurchaseAmountValidator 테스트", () => {
+  test("구입 금액이 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      PurchaseAmountValidator.validate("abc");
+    }).toThrow("[ERROR] 구입 금액은 숫자여야 합니다.");
+  });
+});
